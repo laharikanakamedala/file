c1ce6356f483b7b01338458bd229718ac163648b
laharikanakamedala <93112331+laharikanakamedala@users.noreply.github.com>
2021-11-05 16:27:35 +0530

    Create world.java

---
+++ world.java
@@ -0,0 +1,8 @@
+/**
+* The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+@@ -36,4 +6,4 @@
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+} 
+}
